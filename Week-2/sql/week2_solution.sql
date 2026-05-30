c0ce96a8c927fbf76bfddd908761fb971fee1fa7
Ravi Kumar <ravibseb96@gmail.com>
2026-05-30 22:16:05 +0530

    week-2 task started

---
+++ Week-2/sql/week2_solution.sql
@@ -0,0 +1,15 @@
+USE celebal_week2;
+
+-- =====================
+-- SECTION A
+-- =====================
+
+-- Q1: Display all customers
+
+SELECT * FROM customers;
+
+
+
+Take screenshot.
+
+Add query to SQL file.
